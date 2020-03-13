6c3efd99a443cc3f640e9a7e28501631cbbb5f76
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-03-13 12:22:16 -0400

    hash ex 1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,6 +13,33 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
+    #[2,4,6]  10
+
+#loop through weights
+    for i in range(0, len(weights)):
+        
+        w= weights[i]
+
+
+        num = hash_table_retrieve(ht, limit-w)
+
+#if it matches
+        if num is not None:
+            first_num = max(i, num)
+            sec_num = min(i, num)
+            print("num", num)
+            print("first", first_num)
+            print("sec", sec_num)
+
+            return (first_num, sec_num)
+        else:
+            hash_table_insert(ht, w, i)
+        # if weights[i] in ht:
+        #     return (ht[weights[i]], weights[i])
+        #     print(ht[weights[i]], weights[i])
+        # else:
+        #     ht[limit - weights[i]] = weights[i]
+
     return None
 
 
